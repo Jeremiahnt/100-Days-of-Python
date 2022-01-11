cbc69b1c3bb5fbd55befe1ad15f80a119a22d1e1
Jeremiah <83379118+Jeremiahnt@users.noreply.github.com>
2022-01-10 16:22:28 -0800

    Add files via upload
    This was the first project that I did on my first day, practicing using the input() function with Python and using it in tandem with Python's print and variable functions to create a neat program that makes you a cool band name based on two inputs, that are expected to be a city name and your pet's name.

---
+++ Day 1 - Band name generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the super cool Band Name Generator.\n")
+city = input("What is the name of the city you grew up in?\n" + "**************************************************" + "\n")
+pet_name = input("\n" + "What is your pet's name?\n" + "**************************************************" + "\n")
+
+#end of reading user input
+
+print("Your band's name is: " + city + " " + pet_name)
+
+#prints the user's band name
